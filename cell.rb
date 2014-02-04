185d9daad3f3d96cb887a4654d429a8af985bc10
Jacob Murphy <murphorum@gmail.com>
2014-02-03 22:14:51 -0500

    all but create

--- cell.rb
+++ cell.rb
@@ -1,27 +1,51 @@
 # Author: Jacob Murphy
 
+require 'matrix'
+
 class Cell
-	attr_accessor :neighbors, :walls, :visited, :row, :col
+	attr_accessor :neighbors, :walls, :visited, :distance, :parent
 	def initialize x, y
 		@row = x
 		@col = y
 
 		@neighbors = {top: nil, right: nil, bottom: nil, left: nil}
-		@walls = {top: true, right: true, bottom: true, left: true}
+		@walls =     {top: true, right: true, bottom: true, left: true}
 
+		@parent = nil
+		@distance = (x*y) + 10 # ensures the distance is greater than the number of cells in the maze
 		@visited = false
 	end
 
 	def coordinates
-		[@row, @col]
+		{x: @row.to_i, y: @col.to_i}
 	end
 
 	def travelable_neighbors
 		available = []
 		@walls.each do |key, val|
-			available << @neighbors[key] unless @walls[val]
+			available << @neighbors[key] unless val or @neighbors[key].nil?
 		end
 		available
+	end
+
+	def add_information cell_matrix, wall_matrix
+			@neighbors[:left]   = cell_matrix[ @row, @col - 1 ] unless @col == 0
+			@neighbors[:top]    = cell_matrix[ @row - 1, @col ] unless @row == 0
+			@neighbors[:right]  = cell_matrix[ @row, @col + 1 ] unless @col == (cell_matrix.to_a)[0].length-1
+			@neighbors[:bottom] = cell_matrix[ @row + 1, @col ] unless @row == (cell_matrix.to_a).length-1
+
+			# 0 represents no wall, 1 represents a wall
+			@walls[:top]    = ( wall_matrix[ @row*2    ][ @col*2 +1 ].to_i == 0) ? false : true
+			@walls[:bottom] = ( wall_matrix[ @row*2 +2 ][ @col*2 +1 ].to_i == 0) ? false : true
+			@walls[:right]  = ( wall_matrix[ @row*2 +1 ][ @col*2 +2 ].to_i == 0) ? false : true
+			@walls[:left]   = ( wall_matrix[ @row*2 +1 ][ @col*2    ].to_i == 0) ? false : true
+
+	end
+
+	def reset
+		@walls.each { |key,val| @walls[key] = true }
+		@distance = @row * @col +10
+		@visited = false
 	end
 
 end
--- maze.rb
+++ maze.rb
@@ -1,5 +1,6 @@
 # Author: Jacob Murphy
 
+require 'matrix'
 require_relative 'cell'
 
 class Maze
@@ -10,11 +11,17 @@ class Maze
 		@num_cols = (y*2)+1
 
 		@wall_matrix = []
+		@cell_matrix = Matrix.build(x, y) {|row, col| Cell.new(row, col) }
 	end
 
 	# TODO: add validity check
 	def load str
 		@wall_matrix = str.to_s.split('').each_slice(@num_cols).to_a
+
+		# load the cell matrix
+		@cell_matrix.each do |cell|
+			cell.add_information @cell_matrix, @wall_matrix
+		end
 	end
 
 	def display
@@ -26,14 +33,44 @@ class Maze
 		end
 	end
 
+	
 	def solve begX, begY, endX, endY
-		trace begX, begY, endX, endY
+		result = trace begX, begY, endX, endY
+		if result.nil?
+			puts "\nThere is no possible way to get to [#{endX}, #{endY}] from the starting position that you gave."
+		else
+			puts "There is a possible solution getting from to [#{begX}, #{begY}] to [#{endX}, #{endY}]."
+		end
 	end
 
+	
 	def trace begX, begY, endX, endY
+		q = [] # to be used as a queue in a breadth-first search
+
+		start_cell = @cell_matrix[begX, begY]
+		q.push start_cell # enqueue
+
+		solution = find_maze_path q, endX, endY
+
+		start_cell.parent = nil
+		if solution.nil?
+			return false
+		else
+			print_path solution
+		end
 	end
 
+	# TODO - make this
+	# based off of http://www.mazeworks.com/mazegen/mazetut/
 	def redesign
+		options = [:top, :left, :right, :bottom]
+		# reset all information in the cells
+		@cell_matrix.each{ |cell| cell.reset }
+		temp_options = temp_options.initialize_copy(options)
+
+		option_num = rand(options.size)
+
+
 	end
 
 
@@ -54,6 +91,41 @@ class Maze
 			end
 		end
 	end
+
+	def find_maze_path q, endX, endY
+		dist = 0   # tracks how many steps currently into the maze we are
+		# use a breadth first search to find the shortest path
+		until q.empty?
+			dist += 1
+			current_cell = q.shift # dequeue
+			current_cell.travelable_neighbors.each do |neighbor|
+				unless neighbor.visited and neighbor.distance < dist
+					q.push neighbor  # enqueue
+					set_neighbor neighbor, current_cell, dist
+					return neighbor if neighbor.coordinates[:x].to_i == endX.to_i and neighbor.coordinates[:y].to_i == endY.to_i
+				end
+			end
+		end
+		nil # return nil if no valid path is found
+	end
+
+	def set_neighbor neighbor, parent, dist
+		neighbor.parent = parent
+		neighbor.visited = true
+		neighbor.distance = dist
+	end
+
+	def print_path end_cell
+		path = []
+		until end_cell.nil?
+			path.unshift end_cell
+			end_cell = end_cell.parent
+		end
+
+		path.each { |cell| print "[#{cell.coordinates[:x]}, #{cell.coordinates[:y]}] -> "}
+		print "END"
+	end
+
 end
 
 
@@ -63,3 +135,5 @@ m.load "111111111100010001111010101100010101101110101100000101111011101100000101
 
 #puts m.wall_matrix.to_s
 m.display
+
+puts m.trace 0,0,3,3
